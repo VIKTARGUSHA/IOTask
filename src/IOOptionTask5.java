4c24057e3742d6bd82afcd593abe9002a1a45aab
VAbukhovich <victorgusha1@gmail.com>
2022-02-18 16:02:19 +0300

    17.02.2022work

--- src/IOOptionTask5.java
+++ src/IOOptionTask5.java
@@ -1,11 +1,27 @@
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringReader;
+import java.io.*;
+import java.util.ArrayList;
 
 public class IOOptionTask5 {
     public static void main(String[] args) throws IOException {
-        StringReader stringReader = new StringReader("studentList.txt");
-        //System.out.println(stringReader.ready());
-        PrintWriter printWriter = new PrintWriter();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("studentsList.txt"));
+        String empty = bufferedReader.readLine();
+        String student = bufferedReader.readLine();
+        StringReader stringReader = new StringReader(student);
+        char[] arStud = new char[98];
+        stringReader.read(arStud, 0, 98);
+        System.out.println(arStud);
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("studentList.txt"));
+        StringWriter stringWriter = new StringWriter();
+        ArrayList<Character> arScoresStudent = new ArrayList<>();
+//        for (char anyCharArStud : arStud){
+//            if(anyCharArStud == 1 || anyCharArStud == 2 || anyCharArStud == 3 )
+//        }
+        stringWriter.write(arStud);
+        String [] stuAr = student.split(" ");
+        bufferedWriter.write(stringWriter.toString());
+        bufferedReader.close();
+        bufferedWriter.close();
+
     }
 }
